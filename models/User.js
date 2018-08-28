fe49c4d6c9b9f3157684780c939330e379d3cb1f
Ian Robinson <ian@ianrobinson.net>
2018-08-28 08:57:26 -0700

    Add models and set api routes for new user registration

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Creat Schema
+const UserSchema = new Schema({
+	// What is our User collection going to be made of?
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	avatar: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+// Load the User Model
+const User = require('../models/User.js');
+
 // @route 	GET api/users/test
 // @desc 		Tests post route
 // @access 	Public 
 router.get('/test', (req, res) => res.json({msg: "Yo! users.js Works!"}));
+
+// @route 	GET api/users/register
+// @desc 		Register user
+// @access 	Public 
+router.post('/register', (req, res) => 
+	// now we can use any Mongoose commands
+	User.findOne( { email: req.body.email })
+		.then(user => {
+			if (user) {
+				return res.status(400).json({ email: 'Email Already Recorded' });
+			} else {
+				const newUser = new User({
+					name: req.body.name,
+					email: req.body.email,
+					avatar: // Add Gravatar Here...,
+					password: req.body.password,
+				});
+			}
+		})
+	res.json({msg: "Yo! users.js Works!"})
+);
 
 module.exports = router;
