2e625ee3844408b7e0273af339ba7c69cc396010
Ian Robinson <ian@ianrobinson.net>
2018-08-28 11:11:04 -0700

    Use json web tokens to pass encrypted user login confirmation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
 	mongoURI: 'mongodb://ian:ianrobinson1@ds137102.mlab.com:37102/open-social',
+	secretOrKey: 'secret',
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load the User Model
 const User = require('../../models/User');
@@ -73,7 +75,21 @@ router.post('/login', (req, res) => {
 			bcrypt.compare(password, user.password)
 				.then(isMatch => {
 					if (isMatch) {
-						res.json({msg: "Success"});
+						// User entered correct email and password
+						// Create JWT payload
+						const payload = { id: user.id, name: user.name, avatar: user.avatar };
+						// Sign jsonwebtoken - 
+						jwt.sign(
+							payload, 
+							keys.secretOrKey, 
+							{ expiresIn: 3600 }, 
+							(err, token) => {
+								res.json({
+									success: true,
+									token: 'Bearer' + token,
+								});
+							}
+						);
 					} else {
 						return res.status(400).json({password: 'Incorrect Password'});
 					}
