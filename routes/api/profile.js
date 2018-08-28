5161f5ee075336404508b3b13bf75aca326a14bd
Ian Robinson <ian@ianrobinson.net>
2018-08-28 15:05:14 -0700

    Add api/profile/handle route to profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -33,6 +33,24 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 		.catch(err => res.stats(404).json(err));
 });
 
+// @route 	GET api/profile/handle/:handle
+// @desc 		Get profile by handle
+// @access 	Public
+router.get('/handle/:handle', (req, res) => {
+	const errors = {};
+	Profile.findOne({ handle: req.params.handle })
+		.populate('user', ['name', 'avatar'])
+		.then( profile => {
+			if (!profile) {
+				errors.noprofile = "There is no profile for this user";
+				res.status(404).json(errors);
+			}
+			
+			res.json(profile);
+		})
+		.catch(err => res.status(404).json(err));
+});
+
 // @route 	POST api/profile
 // @desc 		Create or edit user profile
 // @access 	Private
