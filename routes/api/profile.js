24da9757d2e4db50b637a9ed5206ffc10ef8d848
Ian Robinson <ian@ianrobinson.net>
2018-08-28 18:24:13 -0700

    Add more profile API routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -33,6 +33,25 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 		.catch(err => res.stats(404).json(err));
 });
 
+// @route 	GET api/profile/all
+// @desc 		Get all profiles
+// @access 	Public
+router.get('/all', (req, res) => {
+	const errors = {};
+	Profile.find()
+		.populate('user', ['name', 'avatar'])
+		.then(profiles => {
+			if (!profiles) {
+				errors.noprofile = "There are no profiles";
+				return res.status(404).json(errors);
+			}
+			res.json(profiles);
+		})
+		.catch(err => {
+			res.status(404).json({ profile: "There is no profile for this user"})
+		});
+});
+
 // @route 	GET api/profile/handle/:handle
 // @desc 		Get profile by handle
 // @access 	Public
